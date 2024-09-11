aa84f073910dd7aa234c370de49a7d49bce96e42
Kirbyreinch <rembrandtmp@hotmail.com>
2024-09-11 13:22:45 -0600

    Uso de useState para mostrar datos

--- swapir/src/App.js
+++ swapir/src/App.js
@@ -1,95 +1,120 @@
 import logo from './navelogo.jpg';
 import './App.css';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+ function App() {
+    const [data, setData] = useState({});
+    const [error, setError] = useState("");
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const result = await axios(
+                  (`https://swapi.dev/api/people/`), 
+                );
+                setData(result.data);
+            } catch (error) {
+                setError(error.message);
+            }
+        };
+  console.log(data.results)
+        fetchData();
+    }, []);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className='h1-header'>
-         Star Wars Api 
-        </h1>
-      </header>
-<body>
-  <div className="App-body">
-  <h2 className='h2-header'>
-     Personajes de Star Wars
-  </h2>
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className='h1-header'>
+           Star Wars Api  
+          </h1>
+        </header>
+  <body>
+    <div className="App-body">
+    <h2 className='h2-header'>
+       Personajes de Star Wars
+    </h2>
+    </div> 
+  
+    <div className="App-characters-bar">
+  <input type='text' id='character' placeholder='Enter character name'></input>
+  <button onClick>Buscar</button>
+  
+  <table className='table-char'>
+  <tr>
+  <th>Nombre</th>
+  <th>Altura</th>
+  <th>Peso</th>
+  <th>Color de cabello</th>
+  <th>Color de piel</th>
+  <th>Color de ojos</th>
+  <th>Fecha de nacimiento</th>
+  <th>Genero</th>
+  <th>Planeta de nacimiento</th>
+  </tr>
+  <tr>
+
+    
+  <td>{data.name}</td>
+  <td>{data.height}</td>
+  <td>{data.mass}</td>
+  <td>{data.hair_color}</td>
+  <td>{data.skin_color}</td>
+  <td>{data.eye_color}</td>
+  <td>{data.birth_year}</td>
+  <td>{data.gender}</td>
+  <td>{data.homeworld}</td>
+  </tr>
+  </table>
   </div> 
-  <div className="App-characters-bar">
-<input type='text' id='character' placeholder='Enter character name'></input>
-<button onClick={fetchData}>Buscar</button>
-
-
-<table className='table-char'>
-<tr>
-<th>Nombre</th>
-<th>Altura</th>
-<th>Peso</th>
-<th>Color de cabello</th>
-<th>Color de piel</th>
-<th>Color de ojos</th>
-<th>Fecha de nacimiento</th>
-<th>Genero</th>
-<th>Planeta de nacimiento</th>
-</tr>
-
-
-<tr>
-<td></td>
-<td></td>
-<td></td>
-<td></td>
-<td></td>
-<td></td>
-<td></td>
-<td></td>
-<td></td>
-</tr>
 
 
 
-</table>
+  <table className='table-movies'>
+  <tr>
+  <th>Peliculas Relacionadas</th>
+ 
+  </tr>
+  <tr>
+  <td></td>
 
+  </tr>
+  </table>
+ 
 
-
-</div> 
-</body>
-  </div>
-  );
+  </body>
+    </div>
+    );
 }
-
-
-
-
-
-
-
-
-fetchData();
-async function fetchData() {
-  try{
-    const character = document.getElementById("character").value.toLowerCase();
-    const response = await fetch(`https://swapi.dev/api/${character}`);
-    if(!response.ok){
-        throw new Error("Could not fetch resource");
-    }
-    const data = await response.json();
-    const nombre= data.name;
-    console.log(data);
-    console.log(nombre);
-}
-catch(error){
-    console.error(error);
-}
-}
-
-
-
-
-
-
-
 
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- swapir/src/index.js
+++ swapir/src/index.js
@@ -25,4 +25,3 @@ root.render(
 
 reportWebVitals();
 
-//fetch normalito
