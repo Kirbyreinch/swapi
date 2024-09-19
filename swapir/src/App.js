8172c64f3edf02efaf2681140d6a3ec825f0a02a
Kirbyreinch <rembrandtmp@hotmail.com>
2024-09-19 13:24:18 -0600

    Homeworld funcional, tablas aun en proceso y planeacion de paginacion

--- swapir/src/App.js
+++ swapir/src/App.js
@@ -6,22 +6,24 @@ import logo from './navelogo.jpg';
 
 
 function App() {
+
 const [data, setData] = useState({});
-const [searchQuery, setSearchQuery] = useState(""); 
+const [Alldata, setAllData] = useState({});
+const [searchQuery, setSearchQuery] = useState(''); 
 const [films, setFilms] = useState([]);
-const [homeworld, setHomeworld] = useState([]);
 const [vehicles, setVehicles]= useState([]);
-const [starships, setStarships]= useState([]);
+const [homeworld, setHomeworld]= useState('');
 
 
-
+//Peticion Api
 const fetchData = async () => {
 try {
-const result = await axios(
-`https://swapi.dev/api/${searchQuery}`
+    const result = await axios(
+    `https://swapi.dev/api/${searchQuery}`
 );
 const characterData = result.data;
 setData(characterData); 
+
 
 
 
@@ -39,175 +41,122 @@ const vehiclesName = vehiclesresponse.map(vehicle => vehicle.data.name);
 setVehicles(vehiclesName); 
 
 
-//Peticiones vehiculos
-const starshipsrequest = characterData.starships.map(starshipsUrl => axios.get(starshipsUrl));
-const starshipsresponse = await Promise.all(starshipsrequest); 
-const starshipsName = starshipsresponse.map(starship => starship.data.name); 
-setStarships(starshipsName); 
+
+//Peticiones homeworld
+const homeworldresponse = await axios.get(characterData.homeworld); 
+setHomeworld(homeworldresponse.data.name); 
 
 
-
-
-
+//peticiones naves pendiente(hasta que funcione la tabla vehiculos...)
 
 } catch (error) {
-
 }
-
-
-
-
 };
-
-
-
-
-
 
 return (
 <div className="App">
-<header className="App-header">
-<img src={logo} className="App-logo" alt="logo" />
-<h1 className='h1-header'>
-Star Wars Api
-</h1>
-</header>
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <h1 className='h1-header'>
+    Star Wars Api
+    </h1>
+  </header>
+
 <div className="App-body">
-<h2 className='h2-header'>
-Personajes de Star Wars
-</h2>
+  <h2 className='h2-header'>
+    Personajes de Star Wars
+  </h2>
 <div className="App-characters-bar">
-<input type='text'
-id='character'
-placeholder='Enter Character name'
-value={searchQuery}
-onChange={(e) => setSearchQuery(e.target.value)}
-/>
-<button onClick={fetchData}>Buscar</button> {}
+    <input type='text' id='character' placeholder='Enter Character name' value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}/>
+    <button onClick={fetchData}>Buscar</button> {}
 </div>
 
 
 
 {data.name && (
 <table className='table-char'>
-<thead>
-<tr>
-<th>Nombre</th>
-<th>Altura</th>
-<th>Peso</th>
-<th>Color de cabello</th>
-<th>Color de piel</th>
-<th>Color de ojos</th>
-<th>Fecha de nacimiento</th>
-<th>Género</th>
-<th>Planeta de nacimiento</th>
-</tr>
-</thead>
-<tbody>
-<tr>
-<td>{data.name}</td>
-<td>{data.height}</td>
-<td>{data.mass}</td>
-<td>{data.hair_color}</td>
-<td>{data.skin_color}</td>
-<td>{data.eye_color}</td>
-<td>{data.birth_year}</td>
-<td>{data.gender}</td>
-<td>{data.homeworlds}</td>
+  <thead>
 
-</tr>
-</tbody>
+
+  <tr>
+    <th>Nombre</th>
+    <td>{data.name}</td>
+  </tr>
+
+  <tr>
+    <th>Altura</th>
+    <td>{data.height}</td>
+  </tr>
+
+  <tr>
+    <th>Peso</th>
+    <td>{data.mass}</td>
+  </tr>
+
+  <tr>
+    <th>Color de cabello</th>
+    <td>{data.hair_color}</td>
+  </tr>
+
+  <tr>
+    <th>Color de piel</th>
+    <td>{data.skin_color}</td>
+  </tr>
+
+  <tr>
+    <th>Color de ojos</th>
+    <td>{data.eye_color}</td>
+  </tr>
+
+  <tr>
+    <th>Fecha de nacimiento</th>
+    <td>{data.birth_year}</td>
+  </tr>
+
+  <tr>
+    <th>Género</th>
+    <td>{data.gender}</td>
+  </tr>
+
+  <tr>
+    <th>Planeta de nacimiento</th>
+    <td>{homeworld}</td>
+  </tr>
+
+
+</thead>
 </table>
 )}
 
 
-
-{films.length  > 0  &&  (
+{films.length  > 0  &&  ( 
 <table className='table-movies'>
 <thead>
-<tr>
-<th>Películas Relacionadas</th>
-<th>Vehiculos Relacionados</th>
-</tr>
+    <tr>
+    <th>Películas Relacionadas</th>
+    <th>Vehiculos Relacionados</th>
+    </tr>
 </thead>
+<tbody>
 
-
+<tr>
 {films.map((film, index) => (
-<tr key={index}>
+  <tr>
 <td>{[index+1," - ",film]}</td>
+</tr>
+))}
 
-
-
-
-{vehicles.length > 0 && (
-  <tbody>
-  {vehicles.map((vehicle, index) => (
-  <tr key={index}>
-  <td>{[index+1," - ",vehicle]}</td>
-  </tr>
+{vehicles.map((vehicle, indexs) => (
+  <td>{[indexs+1," - ",vehicle]}</td>
   ))}
-  </tbody>
-  )}
-  
+
 </tr>
-))}
-
-
-
-
-
-
-
-</table>
-)}
-
-
-
-
-
-
-
-
-{starships.length > 0 && (
-<table className='table-starships'>
-<thead>
-<tr>
-<th>Naves Relacionadas</th>
-</tr>
-</thead>
-<tbody>
-{starships.map((starship, index) => (
-<tr key={index}>
-<td>{[index+1," - ",starship]}</td>
-</tr>
-))}
 </tbody>
 </table>
-)}
+)} 
 
 
 
-
-
-
-
-
-{homeworld.length > 0 && (
-<table className='table'>
-<thead>
-<tr>
-<th>mundo Relacionado</th>
-</tr>
-</thead>
-<tbody>
-{homeworld.map((homeworlds, index) => (
-<tr key={index}>
-<td>{[index+1," - ",homeworlds]}</td>
-</tr>
-))}
-</tbody>
-</table>
-)}
 
 
 
